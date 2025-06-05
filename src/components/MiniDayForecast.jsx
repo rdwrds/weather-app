2492c2b157d8d1cf6644f2e6b98e6ba4220978b3
rdwrds <r.edwards1492@gmail.com>
2025-06-04 19:52:50 -0500

    bad size handling

--- src/components/MiniDayForecast.jsx
+++ src/components/MiniDayForecast.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import useWindowDimensions from "../useWindowDimensions";
+
 const MiniDayForecast = ({ day, getDecal }) => {
+  const { height, width } = useWindowDimensions();
+
+  let size;
+  if (width < 600) {
+    size = 20;
+  } else {
+    size = 32;
+  }
+
   return (
-    <div className="h-fit w-fit rounded-lg bg-cyan-800/55 p-2.5 text-center drop-shadow-md/35">
-      {getDecal(day.shortForecast, 30)}
+    <div className="my-auto h-fit w-fit rounded-lg bg-cyan-800/55 p-2.5 text-center drop-shadow-md/35">
+      {getDecal(day.shortForecast, size)}
       <p className="text-md">
         {day.temperature}
         {"\u00B0"}
